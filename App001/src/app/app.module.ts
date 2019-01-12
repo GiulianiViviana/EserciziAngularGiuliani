d2d7092ecc89038829b9843d92585770465d9fce
Giuliani <giuliani.viviana@iisgalvanimi.edu.it>
2019-01-12 14:24:14 +0000

    Esercizi su Variabili e Array completi

--- App001/src/app/app.module.ts
+++ App001/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { App01Component } from './app01/app01.component';
+import { UserItemComponent } from './user-item/user-item.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    App01Component
+    App01Component,
+    UserItemComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule
