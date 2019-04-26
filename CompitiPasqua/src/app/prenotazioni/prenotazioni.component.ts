4554dd25afeec2561da62c0162144701af5f96b0
EC2 Default User <ec2-user@ip-172-31-42-81.ec2.internal>
2019-04-26 12:45:35 +0000

    Sistemato con FormGroup e FormControl ma bottone non funziona

--- CompitiPasqua/src/app/prenotazioni/prenotazioni.component.ts
+++ CompitiPasqua/src/app/prenotazioni/prenotazioni.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PRENOTAZIONI } from '../mock-prenotazioni';
 import { Prenotazione } from '../prenotazione';
-
+import { FormBuilder,FormGroup,Validators} from '@angular/forms';
 @Component({
   selector: 'app-prenotazioni',
   templateUrl: './prenotazioni.component.html',
@@ -11,26 +11,32 @@ export class PrenotazioniComponent implements OnInit {
 
   prenotazioni = PRENOTAZIONI;
   selectedPrenotazione: Prenotazione;
-
-  constructor() { }
-
+  myForm: FormGroup;
+ constructor(fb: FormBuilder) { 
+  this.myForm = fb.group({
+    'nome': ['', Validators.required],
+    'cognome': ['', Validators.required],
+    'indirizzo': ['', Validators.required],
+    'telefono': [''],
+    'DataPrenotazione': ['', Validators.required],
+    'OraPrenotazione': [''],
+  });
+ }
   ngOnInit() {
   }
 
   onSelect(prenotazione: Prenotazione): void {
     this.selectedPrenotazione = prenotazione;
   }
-
-  onAddPrenotazione(nome: HTMLInputElement, cognome: HTMLInputElement,indirizzo: HTMLInputElement,telefono: HTMLInputElement,
-  DataPrenotazione: HTMLInputElement,OraPrenotazione: HTMLInputElement)
-  {
+  ngSubmit(){
       let pren : Prenotazione = new Prenotazione();
-      pren.nome = nome.value;
-      pren.cognome = cognome.value;
-      pren.indirizzo = indirizzo.value;
-      pren.telefono = Number(telefono.value);
-      pren.DataPrenotazione = DataPrenotazione.value;
-      pren.OraPrenotazione = OraPrenotazione.value;
+      pren.nome = this.myForm.controls['nome'].value;
+      pren.cognome = this.myForm.controls['cognome'].value;
+      pren.indirizzo = this.myForm.controls['indirizzo'].value;
+      pren.telefono = Number(this.myForm.controls['telefono'].value);
+      pren.DataPrenotazione = this.myForm.controls['DataPrenotazione'].value;
+      pren.OraPrenotazione = this.myForm.controls['OraPrenotazione'].value;
       this.prenotazioni.push(pren);
-  }
+  
+}
 }
